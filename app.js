95353d82bc0557fffb272227b6a4e34dbab27b0d
UzairRizwanadmani <uzairadmani38@gmail.com>
2024-08-25 17:44:26 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,118 @@
+//  Q1
+alert("Welcome to our Website");
+// Q2
+alert("Error! Please Enter the valid password");
+//Q3
+document.write("Error! Please Enter the valid password \n try again");
+//Q4
+alert("message 1");
+alert("message 2");
+alert("message 3");
+//Q5
+function greet(){
+    alert("welcome to our website");
+}
+//Q7
+var username = ("");
+console.log(username);
+// //Q8
+var myName = ("Uzair Rizwan");
+console.log(username);
+// //Q9
+var js =("hello world");
+alert(js)
+// //Q10
+var stuname = ("Ali");
+var stuage = ("18");
+var stuquali = ("inter");
+alert("Name : "+ stuname);
+alert("age : "+ stuage);
+alert("Qualification : "+ stuquali);
+// //Q11
+var email = ("uzairadmani38@gmail.com");
+alert("Email : "+email);
+// //Q12
+console.log("Email : "+email);
+//Q13
+var ag = ("sample text")
+console.log(ag);alert(ag);
+// Q14
+var age = (15);
+alert(age);
+//Q15
+var visit =0; visit++;
+document.write("\n you have visited this sites " + visit + " times");
+// //Q16
+var birthyear = (2007);
+console.log(birthyear);
+// //Q17
+var vname = ("taha");
+var product = ("perfumes");
+var quantity = ("11");
+console.log(vname + " orderd " + quantity +" "+ product + " on XYZ Clothing" );
+// //Q18
+var a = 10, b = 25 , c = 65;
+//Q19
+// illegal variable
+var alert = ("hello")
+var var = ("hello")
+var function = ("hello")
+var console.log = ("hello")
+var let = ("hello")
+// // llegal variable
+var a = ("hello")
+var l = ("hello")
+var rt = ("hello")
+var t = ("hello")
+var ale = ("hello")
+//Q20
+console.log("Rules for naming JS variables");
+console.log("Variable names can only contain letters, numbers, dollar signs, and underscores. For example: $my_1stVariable and For.");
+console.log("Variables must begin with a dollar sign ($), underscore (_), or a letter. For example: $name, _name, or name.");
+console.log("Variable names are case-sensitive.");
+console.log("Variable names should not be JavaScript reserved keywords.");
+// Q21
+var num1 = +prompt("First number to add ")
+var num2 = +prompt("second number to add ")
+var num3 = (num1 + num2)
+console.log("The Sum Is : " + num3)
+// //Q22
+var num4 = +prompt("First number for Subtraction,multiplication,division,modulus ")
+var num5 = +prompt()
+var num6 = (num4 - num5)
+var num7 = (num4 * num5)
+var num8 = (num4 / num5)
+var num9 = (num4%num5)
+console.log("The Subtraction Is : " + num6)
+console.log("The multiplication Is : " + num7)
+console.log("The division Is : " + num8)
+console.log("The modulus Is : " + num9)
+// //Q23
+var num;
+console.log("Value after variable decleration is: " + num)
+num = 15
+console.log("Intial value of the variable: " + num)
+num++
+console.log("Value of variable after increment: " + num)
+num += 7
+console.log("Value of variable after addition: " + num)
+num--
+console.log("Value of variable after decreament: " + num)
+var remainder = num % 3
+console.log("remainder after dividing 22 by 3: " + remainder)
+// Q24
+var price = 600
+var total_price = price * 5
+console.log("Price for 5 tickets is " + total_price)
+// Q25
+var m = 2
+console.log(m+"x1: " + m*1)
+console.log(m+"x2: " + m*2)
+console.log(m+"x3: " + m*3)
+console.log(m+"x4: " + m*4)
+console.log(m+"x5: " + m*5)
+console.log(m+"x6: " + m*6)
+console.log(m+"x7: " + m*7)
+console.log(m+"x8: " + m*8)
+console.log(m+"x9: " + m*9)
+console.log(m+"x10: " + m*10)
